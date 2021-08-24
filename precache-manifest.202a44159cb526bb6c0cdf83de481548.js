64396e8614f8ad29ff9e5b7fdb9bf789d29d7eed
kroy <meetkallolroy@gmail.com>
2021-08-24 23:13:50 +0600

    Updates

---
+++ precache-manifest.202a44159cb526bb6c0cdf83de481548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853942945d3b97c8c110dd9200cdb3a0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "37484fb7d36b2598cbca",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "884bf174a344463742c6",
+    "url": "./static/js/2.9bc1f99a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.9bc1f99a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37484fb7d36b2598cbca",
+    "url": "./static/js/main.164abba9.chunk.js"
+  },
+  {
+    "revision": "565f96ff9a9c9fea7657",
+    "url": "./static/js/runtime-main.6f4b282b.js"
+  }
+]);
